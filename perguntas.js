648970031b150497ab1c577938a37b1fe36b2033
kaua0511 <00001097182198sp@aluno.educacao.sp.gov.br>
2024-11-27 08:23:50 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'HTML é uma linguagem de programação? ',
+    'Não, HTML é uma linguagem de texto.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Russia?',
+    'A capital da Russia é Moscou'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma API?',
+    'Uma API serve para facilitar a comunicação e a integração entre diferentes sistemas, aplicativos ou serviços.'
+)
+
+criaCartao(
+    'Tecnologia',
+    'O que é tecnologia?',
+    'Tecnologia é um conjunto de conhecimentos, técnicas, habilidades e processos utilizados para desenvolver produtos, serviços ou sistemas que visam facilitar a vida e atender às necessidades humana.'
+)
